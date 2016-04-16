b46b2f1752e617aec65e2a8e3ecc17f6c7232968
valarsu <valar_su@163.com>
2016-04-16 23:16:44 +0800

    解释文件

--- app.js
+++ app.js
@@ -1,41 +1,52 @@
+//通过require加载express path serve-favicon morgan
+// cookie-parser body-parser
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+//加载路由routes/index  routes/users
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-var app = express();
+var app = express();//生成一个express实例
 
-// view engine setup
+
+//设置views文件夹为存放视图文件的目录，及存放文件模板的地方，
+//__dirname为全局变量，存储当前正在执行的脚本所在的目录
+
 app.set('views', path.join(__dirname, 'views'));
+
+//设置视图模板引擎为ejs
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
+//设置/public/favicon.ico 为favicon图标
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//加载日志中间件
 app.use(logger('dev'));
+//加载解析json的中间件
 app.use(bodyParser.json());
+//加载解析urlencoded请求体的中间件
 app.use(bodyParser.urlencoded({ extended: false }));
+//加载解析cookie的中间件
 app.use(cookieParser());
+//设置public文件夹为存放静态文件的目录
+app.use(express.static(path.join(__dirname,'public')));
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
-
+//路由控制器
 app.use('/', routes);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
+// 捕获404错误，并转发到错误处理器
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
+//开发环境下的错误处理器，将错误信息渲染error模板并显示到浏览器中
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -46,8 +57,7 @@ if (app.get('env') === 'development') {
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
+//生产环境下的错误处理器，将错误信息渲染error模板并显示到浏览器中
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
@@ -56,5 +66,5 @@ app.use(function(err, req, res, next) {
   });
 });
 
-
+//导出app实例供其他模块调用
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,12 @@
+/*
+* 生成一个路由实例用来捕获访问主页的GET请求，导出这个路由并在app.js
+* 中通过app.use('/',routes);加载。这样，当访问主页时，
+* 就会调用res.render('index',{title:'Express'});
+* 渲染views/index.ejx模板并显示到浏览器中
+* */
+
+
+
 var express = require('express');
 var router = express.Router();
 
