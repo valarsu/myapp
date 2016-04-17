5b27ecb83feb65637d52fce9bb6ddcf1b7f2f5f8
valarsu <valar_su@163.com>
2016-04-17 21:32:30 +0800

    add pages and set route

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,24 @@ var router = express.Router();
 //
 // module.exports = router;
 module.exports = function (app) {
-  app.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
-  });
+    app.get('/', function(req, res, next) {
+        res.render('index', { title: '主页' });
+    });
+    app.get('/reg', function(req, res, next) {
+        res.render('reg', { title: '注册' });
+    });
+    app.post('/reg',function (req, res) {
+    });
+    app.get('/login', function(req, res, next) {
+        res.render('login', { title: '登录' });
+    });
+    app.post('/login',function (req, res) {
+    });
+    app.get('/post', function(req, res, next) {
+        res.render('post', { title: '发表文章' });
+    });
+    app.post('/post',function (req, res) {
+    });
+    app.get('/logout', function(req, res, next) {
+    });
 };
