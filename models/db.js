f87dfefb7685ec2ffaef1d74f12e2b9a465351cf
valarsu <valar_su@163.com>
2016-04-23 23:46:04 +0800

    sss

--- models/db.js
+++ models/db.js
@@ -3,6 +3,7 @@
  */
 var settings = require('../settings'),
     Db = require('mongodb').Db,
-    Connection = require('mongodb').Server;
+    Connection = require('mongodb').Connection,
+    Server = require('mongodb').Server;
 module.exports = new Db(settings.db, new Server(settings.host, settings.port),
-    {safe : true});
+    {safe: true});
